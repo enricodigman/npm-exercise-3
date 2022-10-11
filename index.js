978e0fb83a9d18594557b410db8fdf4dd2857ec4
Enrico Digman <essdigman@gmail.com>
2022-10-11 09:35:23 +0800

    Implemented prettier module

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const argument = process.argv.slice(2);
 const expression = argument.toString();
 
 if (argument.length === 1) {
-    console.log(parse(expression));
-    console.log(toString(parse(expression)));
-    console.log(toArray(parse(expression)));
-}
-else {
-    console.log('This program only accepts a SINGLE argument');
+  console.log(parse(expression));
+  console.log(toString(parse(expression)));
+  console.log(toArray(parse(expression)));
+} else {
+  console.log('This program only accepts a SINGLE argument');
 }
